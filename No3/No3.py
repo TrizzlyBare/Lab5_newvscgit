ee6caf020a1bd76c9cb9c7456560113247ae58fc
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2024-12-27 17:44:14 +0700

    No3

--- No3/No3.py
+++ No3/No3.py
@@ -1,3 +1,32 @@
+import turtle as t
+
 class Disk(object):
     def __init__(self, name="", xpos = 0, ypos = 0, height = 20, width = 40):
-        pass
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+
+    def showdisk(self):
+
+    def newpos(self, xpos, ypos):
+
+    def cleardisk(self)
+        
+class Pole(object):
+    def __init__(self, name="", xpos = 0, ypos = 0, thick = 10, length = 100):
+        self.pname = name
+        self.stack = []
+        self.toppos = 0
+        self.pxpos = xpos
+        self.pypos = ypos
+        self.pthick = thick
+        self.plength = length
+
+    def showpole(self):
+
+    def pushdisk(self, disk):
+
+    def popdisk(self):
+
