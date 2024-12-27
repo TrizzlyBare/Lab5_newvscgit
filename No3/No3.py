a506640a079f234990464bbfb06756f3bfe9a133
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2024-12-27 18:19:14 +0700

    Done

--- No3/No3.py
+++ No3/No3.py
@@ -1,7 +1,7 @@
 import turtle as t
 
 class Disk(object):
-    def __init__(self, name="", xpos = 0, ypos = 0, height = 20, width = 40):
+    def __init__(self, name="", xpos=0, ypos=0, height=20, width=40):
         self.dname = name
         self.dxpos = xpos
         self.dypos = ypos
@@ -9,13 +9,41 @@ class Disk(object):
         self.dwidth = width
 
     def showdisk(self):
+        t.penup()
+        t.goto(self.dxpos, self.dypos)
+        t.setheading(0)
+        t.pendown()
+        t.begin_fill()
+        for _ in range(2):
+            t.fd(self.dwidth)
+            t.left(90)
+            t.fd(self.dheight)
+            t.left(90)
+        t.end_fill()
 
     def newpos(self, xpos, ypos):
+        self.cleardisk()
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.showdisk()
 
-    def cleardisk(self)
-        
+    def cleardisk(self):
+        t.penup()
+        t.goto(self.dxpos, self.dypos)
+        t.setheading(0)
+        t.pendown()
+        t.color("white")
+        t.begin_fill()
+        for _ in range(2):
+            t.fd(self.dwidth)
+            t.left(90)
+            t.fd(self.dheight)
+            t.left(90)
+        t.end_fill()
+        t.color("black")
+
 class Pole(object):
-    def __init__(self, name="", xpos = 0, ypos = 0, thick = 10, length = 100):
+    def __init__(self, name="", xpos=0, ypos=0, thick=10, length=100):
         self.pname = name
         self.stack = []
         self.toppos = 0
@@ -41,32 +69,39 @@ class Pole(object):
         return None
 
 
-# class Hanoi (Object) :
-#     def __init__ (self,n = 3 , start = "A", workspace = "B" , destination = "C") :
-#         self.startp = Pole(start 0,0)
-#         self.workspacep = Pole(workspace,150,0)
-#         self.destinationp = Pole(destination,300,0)
-#         self.startp.showpole()
-#         self.workspacep.showpole()
-#         self.destinationp.showpole()
+class Hanoi(object):
+    def __init__(self, n=3, start="A", workspace="B", destination="C"):
+        self.startp = Pole(start, 0, 0)
+        self.workspacep = Pole(workspace, 150, 0)
+        self.destinationp = Pole(destination, 300, 0)
+        self.startp.showpole()
+        self.workspacep.showpole()
+        self.destinationp.showpole()
 
-#         for i  in range(n) :
-#             self.startp.pushdisk(Disk("d"+str(i),0,i*150,20,(n-i)*30))
+        for i in range(n):
+            self.startp.pushdisk(Disk("d" + str(i), 0, i * 20, 20, (n - i) * 30))
 
-#     def move_disk(self,start,destination) :
-#         disk = start.popdisk()
-#         destination.pushdisk(disk)
+    def move_disk(self, start, destination):
+        disk = start.popdisk()
+        destination.pushdisk(disk)
 
-#     def move_tower(self,n,s,d,w) :
-#         if n == 1:
-#             self.move_disk(s,d)
-#         else:
-#             self.move_tower(n-1,s,w,d)
-#             self.move_disk(s,d)
-#             self.move_tower(n-1,w,d,s)
+    def move_tower(self, n, s, d, w):
+        if n == 1:
+            self.move_disk(s, d)
+        else:
+            self.move_tower(n - 1, s, w, d)
+            self.move_disk(s, d)
+            self.move_tower(n - 1, w, d, s)
 
-#     def solve(self) :
-#         self.move_tower(3,self.startp,self.destinationp,self.workspacep)
+    def solve(self):
+        self.move_tower(3, self.startp, self.destinationp, self.workspacep)
+        t.done()  # Finish the turtle drawing
 
-# h = Hanoi()
-# h.solve()                              
+
+# Initialize Turtle window
+t.speed(0)  # Set drawing speed to fastest
+t.bgcolor("white")  # Set background color to white
+
+# Solve the puzzle
+h = Hanoi()
+h.solve()
